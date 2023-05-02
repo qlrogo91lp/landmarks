b0a66d0cb66e660c9031b8ae06ad1229fc8fd6a1
yjkim <yjkim@4net.co.kr>
2023-05-02 16:41:54 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 윤재 on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
